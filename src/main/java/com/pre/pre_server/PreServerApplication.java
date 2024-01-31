5ee72b62db4023a41ffa04e53fcb1653769bc637
ri-naa <rina288@ewhain.net>
2024-01-31 22:35:03 +0900

    initial setting

---
+++ src/main/java/com/pre/pre_server/PreServerApplication.java
@@ -0,0 +1,13 @@
+package com.pre.pre_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreServerApplication.class, args);
+	}
+
+}
